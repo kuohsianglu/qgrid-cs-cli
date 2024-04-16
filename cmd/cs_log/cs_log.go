a7411608410b9ee69af825dbaed4b8e71c21917c
Kenneth Lu <kuohsianglu@gmail.com>
2024-04-16 14:52:45 +0800

    cmd: refactoring cs log command
    Signed-off-by: Kenneth Lu <kuohsianglu@gmail.com>

--- cmd/cs_log/cs_log.go
+++ cmd/cs_log/cs_log.go
@@ -19,6 +19,103 @@ var (
 	key    string
 )
 
+func request_log() {
+	flag.StringVar(&server, "server", "localhost:6379", "Redis hostname:port")
+	flag.StringVar(&key, "key", "api:stream:request", "Redis Streams key to read from")
+	flag.Parse()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: server,
+	})
+	ctx := context.Background()
+	lastID := "0"
+
+	for {
+		resp, err := rdb.XRead(ctx, &redis.XReadArgs{
+			Streams: []string{key, lastID},
+			Count:   10,
+			Block:   0,
+		}).Result()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(resp) != 1 {
+			log.Fatal("Exactly one stream response is expected")
+		}
+
+		for _, msg := range resp[0].Messages {
+			lastID = msg.ID
+
+			if b, ok := msg.Values["request"].(string); ok {
+				var pl stream.ApiRequestLog
+				if err := proto.Unmarshal([]byte(b), &pl); err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println("=== Request ===")
+				fmt.Println(protojson.Format(&pl))
+				fmt.Println("===============")
+			}
+
+		}
+	}
+}
+
+func meta_log() {
+	flag.StringVar(&server, "server", "localhost:6379", "Redis hostname:port")
+	flag.StringVar(&key, "key", "stream:meta", "Redis Streams key to read from")
+	flag.Parse()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: server,
+	})
+	ctx := context.Background()
+
+	lastID := "0"
+
+	for {
+		resp, err := rdb.XRead(ctx, &redis.XReadArgs{
+			Streams: []string{key, lastID},
+			Count:   10,
+			Block:   0,
+		}).Result()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(resp) != 1 {
+			log.Fatal("Exactly one stream response is expected")
+		}
+
+		for _, msg := range resp[0].Messages {
+			lastID = msg.ID
+
+			if b, ok := msg.Values["up"].(string); ok {
+				var pl stream.UplinkMeta
+				if err := proto.Unmarshal([]byte(b), &pl); err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println("=== UP ===")
+				fmt.Println(protojson.Format(&pl))
+				fmt.Println("==========")
+			}
+
+			if b, ok := msg.Values["down"].(string); ok {
+				var pl stream.DownlinkMeta
+				if err := proto.Unmarshal([]byte(b), &pl); err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println("=== DOWN ===")
+				fmt.Println(protojson.Format(&pl))
+				fmt.Println("============")
+			}
+		}
+	}
+}
+
 func event_log() {
 	flag.StringVar(&server, "server", "localhost:6379", "Redis hostname:port")
 	flag.StringVar(&key, "key", "device:stream:event", "Redis Streams key to read from")
@@ -201,5 +298,9 @@ func main() {
 		event_log()
 	case log_type == "frame":
 		frame_log()
+	case log_type == "meta":
+		meta_log()
+	case log_type == "request":
+		request_log()
 	}
 }
